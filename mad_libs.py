54e7b47a5cf1877b9bbde78d13288713157c0d4e
Kd1006 <118497448+Kd1006@users.noreply.github.com>
2022-11-17 23:07:11 -0500

    Better String formatting

--- mad_libs.py
+++ mad_libs.py
@@ -1,7 +1,6 @@
 color = input("Enter a color:")
-plural_noun = input("Enter a plural number:")
-celebrity= input("Enter a celebrity:")
-
-print("Roses are " + color)
-print(plural_noun + "are blue")
-print("I love " + celebrity )
+plural_noun = input("Enter a plural noun:")
+celebrity = input("Enter a celebrity:")
+print(f'Roses are {color}')
+print(f'{plural_noun} are blue')
+print(f'I love {celebrity}')
